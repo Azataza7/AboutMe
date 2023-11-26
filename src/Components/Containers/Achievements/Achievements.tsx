99e84293f3de278824f4cd7087416d18e27290e1
Azat Kulbaev <kulbaevaazat@gmail.com>
2023-11-27 03:15:24 +0600

    little changes in achieve container

--- src/Components/Containers/Achievements/Achievements.tsx
+++ src/Components/Containers/Achievements/Achievements.tsx
@@ -3,21 +3,37 @@ import React from 'react';
 const Achievements = () => {
   return (
     <div className="achieves text-center fs-5 mt-3 p-3">
-      <div className="text-success">
-        Education
-        <p>Recognized for maintaining a high GPA throughout the academic year).</p>
-        <p>Awarded a full scholarship for academic excellence))</p>
+      <div className="row justify-content-center">
+        <div className="col-sm-8 mb-2 mb-sm-1">
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">Education</h5>
+              <p className="card-text">Recognized for maintaining a high GPA throughout the academic year).</p>
+              <p className="card-text">Awarded a full scholarship for academic excellence))</p>
+            </div>
+          </div>
+        </div>
+        <div className="col-sm-8 mb-2">
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">Sport</h5>
+              <p className="card-text">Bench Press 120 kg</p>
+              <p className="card-text">Dead Lift 210 kg</p>
+            </div>
+          </div>
+        </div>
+        <div className="col-sm-8 mb-2">
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">Business</h5>
+              <p className="card-text">Online shop: Computer components and headphones</p>
+              <p className="card-text">Currently sell candies and bakery</p>
+            </div>
+          </div>
+        </div>
+
       </div>
-      <div className="text-danger">
-        Sport
-        <p> Bench Press 120 kg</p>
-        <p> Dead Lift 210 kg</p>
-      </div>
-      <div className="text-black">
-        Business
-        <p>Online shop: Computer components and headphones</p>
-        <p>Currently sell candies and bakery</p>
-      </div>
+
     </div>
   );
 };
